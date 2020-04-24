d2994e81e3bfbf329eea271d2a7472fde87e9df1
Matt-Stedman-TEC <matt@theengineeringcompany.com>
2020-04-24 14:41:24 +0100

    Improved testing framework to support lists better

--- pycomponents_general_maths/e_log_n/test_e_log_n.py
+++ pycomponents_general_maths/e_log_n/test_e_log_n.py
@@ -9,10 +9,16 @@ import math
 # Tests
 def run_test_func(inputs, outputs):
     test_data = component.test(inputs, outputs)
-
+    tolerance = 0.000001
     for port_data in test_data.values():
         for p_exp, p_got in zip(port_data.expected, port_data.got):
-            assert p_exp["Value"] == pytest.approx(p_got["Value"], rel=0.000001)  # assert at least 0.00001% accurate
+            if isinstance(p_exp, list):
+                for i in range(0, len(p_exp)):
+                    p_exp[i]["Value"] == pytest.approx(p_got[i]["Value"], rel=tolerance)
+            else:
+                assert p_exp["Value"] == pytest.approx(
+                    p_got["Value"], rel=tolerance
+                )  # assert at least 0.00001% accurate
 
 
 def test_e_log_n_ints():
