38431863c43c2ca868eaca1c31536fbb4f2da7e1
Matt-Stedman-TEC <matt@theengineeringcompany.com>
2020-07-23 18:31:01 +0100

    fixed broken test

--- flow_py_library_general_maths/general_maths/trig_cos/test_trig_cos.py
+++ flow_py_library_general_maths/general_maths/trig_cos/test_trig_cos.py
@@ -1,7 +1,7 @@
 import math
 
 from flow.test_framework import FlowTest, flow_test
-from flow_types import base
+from flow_types import base, eng
 
 from flow_py_library_general_maths.general_maths.trig_cos.flow_trig_cos import inports, outports
 from flow_py_library_general_maths.util.utils_tests import basic_test_eval
@@ -17,7 +17,7 @@ def run_test_func(inputs, outputs, flow: FlowTest):
 
 def test_cos_double(flow: FlowTest):
     inputs = {
-        inports[0]: [base.Double(0.3)],
+        inports[0]: [eng.Angle(0.3)],
     }
 
     outputs = {outports[0]: [base.Double(math.cos(0.3))]}
