78ebf3249a47561b2879ad352cb5c8233939feeb
Harry <harry@theengineeringcompany.com>
2021-06-28 23:05:55 +0100

    feat: Sin component updated to dynamic

--- flow_package_maths/advanced/trigonometry/sin/sin.py
+++ flow_package_maths/advanced/trigonometry/sin/sin.py
@@ -1,32 +1,59 @@
-import math
-from typing import cast
+from math import pi, sin
+from typing import List
 
-from flow import Component, Definition, Inport, Outport
-from flow_types import base, unions
+from flow import Ports, Process, SelectField, Settings, Setup
+from flow.testing import ComponentTest
+from flow_types import base
 
-# ports
-angle = Inport(id="angle", types=unions.Number, multi_connection=False)
-result = Outport(id="result", types=[base.Double])
+input_types: List = ["degrees", "radians", "gradians"]
 
-# comp definition
-definition = Definition(inports=[angle], outports=[result])
+settings = Settings()
+settings.add_setting(id="rad_or_deg_or_grad", field=SelectField(options=input_types))
+
+ports = Ports()
+ports.add_inport(id="angle", types=[base.Double, base.Int, base.Bool])
+ports.add_outport(id="result", types=[base.Double])
 
 
-def process(component: Component):
+def setup(component: Setup):
 
-    if not component.has_data():
-        return
+    input_type = str(component.get_setting("rad_or_deg_or_grad"))
 
-    # get inports data
-    # note angle_val is in radians
-    angle_val: float = cast(base.Double, component.get_data(angle)).value
+    if input_type == "degrees":
+        angle_conversion = pi / 180  # noqa: WPS432
+
+    if input_type == "gradians":
+        angle_conversion = pi / 200  # noqa: WPS432
+
+    if input_type == "radians":
+        angle_conversion = 1
+
+    component.set_variable("angle_conversion", angle_conversion)
+
+
+def process(component: Process):
+
+    angle_conversion: float = component.get_variable("angle_conversion")
+
+    angle_in = float(component.get_data("angle"))
+
+    angle_rad = angle_in * angle_conversion
 
     # sin
-    res = math.sin(angle_val)
-
-    # Log
-    # angle_val_deg = angle_val * 180 / math.pi  # noqa: WPS432
-    # component.log(log_level=LogLevel.DEBUG, message=f"sin({angle_val}rad or {angle_val_deg}°) gives {res}.")
+    result = sin(angle_rad)
 
     # send message to outports
-    component.send_data(base.Double(res), result)
+    component.send_data(base.Double(result), "result")
+
+
+if __name__ == "__main__":
+    setting_data = {
+        "rad_or_deg_or_grad": base.String("gradians"),
+    }
+
+    inports_data = {
+        "angle": base.Double(80),  # noqa: WPS432
+    }
+
+    outport_value = ComponentTest(__file__).run(inports_data, setting_data)
+    print(outport_value["result"])
