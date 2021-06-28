29923e33808a3b9cf8c1db1a48b901fed6b7c306
Eu Chian <euchian@theengineeringcompany.com>
2021-06-28 22:31:40 +0100

    feat: upgraded add to dynamic add

--- flow_package_maths/arithmetic/add/README.md
+++ flow_package_maths/arithmetic/add/README.md
@@ -1,1 +1,1 @@
-Adds two values together.
+Adds two values together by default. You can add more addition terms by changing `Number of Terms` in the component's settings.
--- flow_package_maths/arithmetic/add/add.py
+++ flow_package_maths/arithmetic/add/add.py
@@ -1,21 +1,43 @@
-from flow import Ports, Process
-from flow_types import base, unions
+from typing import List
 
-# ports
+from flow import IntField, Ports, Process, Settings, Setup
+from flow.testing import ComponentTest
+from flow_types import base
+
+# Ports
 ports = Ports()
-ports.add_inport(id="value1", types=unions.Number)
-ports.add_inport(id="value2", types=unions.Number)
 ports.add_outport(id="result", types=[base.Double])
 
+# Settings
+settings = Settings()
+settings.add_setting(id="terms", field=IntField(min=2), default=base.Int(2))
 
-def process(component: Process) -> None:
 
-    # get inports data
-    value1: float = component.get_data("value1").value
-    value2: float = component.get_data("value2").value
+def setup(component: Setup):
 
-    # add
-    result = value1 + value2
+    terms = int(component.get_setting("terms"))
 
-    # send message to outports
+    inport_ids: List[str] = []
+    for idx in range(terms):
+        inport_id = f"value{idx + 1}"
+        inport_ids.append(inport_id)
+        component.add_inport(name=f"Value {idx + 1}", id=inport_id, types=[base.Double, base.Int, base.Bool])
+
+    component.set_variable("inport_ids", inport_ids)
+
+
+def process(component: Process):
+
+    inport_ids: List[str] = component.get_variable("inport_ids")
+    result = sum(float(component.get_data(inport_id)) for inport_id in inport_ids)
+
     component.send_data(base.Double(result), "result")
+
+
+if __name__ == "__main__":
+
+    setting_data = {"terms": base.Int(3)}
+    inports_data = {"value1": base.Double(2), "value2": base.Double(2), "value3": base.Double(1)}
+
+    outport_value = ComponentTest(__file__).run(inports_data, setting_data)
+    print(outport_value["result"])
